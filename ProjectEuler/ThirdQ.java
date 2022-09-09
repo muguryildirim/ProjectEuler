2ba0243c1536671b29837be4f1e8c1ff403c0d61
muy <109519208+muguryildirim@users.noreply.github.com>
2022-09-09 19:12:13 +0300

    Update ThirdQ.java

--- ProjectEuler/ThirdQ.java
+++ ProjectEuler/ThirdQ.java
@@ -1,7 +1,7 @@
-/**************************************************************
-* The prime factors of 13195 are 5, 7, 13 and 29.
-* What is the largest prime factor of the number 600851475143 ?
-***************************************************************/
+/****************************************************************
+* The prime factors of 13195 are 5, 7, 13 and 29.               *
+* What is the largest prime factor of the number 600851475143 ? *
+*****************************************************************/
 public class ThirdQ {
     private static final long NUMBER = 600851475143L;
 

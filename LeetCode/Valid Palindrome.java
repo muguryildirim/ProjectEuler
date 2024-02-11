64b1b094ece4295172a2d13fce3184a6d110e9ef
Mehmet Uğur Yıldırım <109519208+muguryildirim@users.noreply.github.com>
2024-02-11 19:37:37 +0300

    Valid Palindrome

--- LeetCode/Valid Palindrome.java
+++ LeetCode/Valid Palindrome.java
@@ -1,5 +1,6 @@
 class Solution {
     public static boolean isPalindrome(String s) {
+        //* String Manipulation */
         Stack<Character> stack = new Stack<>();
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
@@ -13,5 +14,26 @@ class Solution {
         }
 
         return s.equals(reversed.toString());
+    }
+}
+
+class Solution {
+    public static boolean isPalindrome(String s) {
+        /** Best Answer for Runtime and Memory */
+        int left = 0, right = s.length() - 1;
+        while(left < right) {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            } 
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            } 
+            if(left < right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
